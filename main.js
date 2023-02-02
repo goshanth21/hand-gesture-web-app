f9c7cd71913a6892d17e488c42d669f79023311c
goshanth21 <107615729+goshanth21@users.noreply.github.com>
2023-02-02 17:26:52 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,67 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality:90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach( '#camera' );
+
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+
+console.log('ml5 version:', ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/xQRttb4CE/model.json',modelLoaded);
+
+function modelLoaded() {
+    console.log('Model Loaded!');
+}
+
+function speak() {
+    var synth = window.speechSynthesis;
+    speak_data = toSpeak;
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+}
+
+function check()
+{
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+    document.getElementById("result_emotion_name").innerHTML = results[0].label;
+    gesture = results[0].label;
+    toSpeak = "";
+    if(results[0].label == "amazing")
+    {
+        toSpeak = "This is looking Amazing";
+        document.getElementById("update_emoji").innerHTML = "&#128076;";
+    }
+    else if(results[0].label == "best")
+    {
+        toSpeak = "All the Best";
+        document.getElementById("update_emoji").innerHTML = "&#128077;";
+    }
+    else if(results[0].label == "victory")
+    {
+        toSpeak = "That was the marvelous Victory";
+        document.getElementById("update_emoji").innerHTML = "&#9996;"
+    }
+    speak();
+    }
+}
